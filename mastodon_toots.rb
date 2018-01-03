f8f365ee7be7171807ea798262e8c6c320c61fb6
lonexstar <lone.s.no9@gmail.com>
2018-01-04 00:28:04 +0900

    日毎のトゥート数を表示する:fire:

--- mastodon_toots.rb
+++ mastodon_toots.rb
@@ -1,5 +1,6 @@
 require 'mastodon'
 require 'dotenv'
+require 'time'
 
 Dotenv.load
 
@@ -10,18 +11,27 @@ LIMIT = 40
 
 client = Mastodon::REST::Client.new(base_url: FRIENDS_NICO_URL, bearer_token: ACCESS_TOKEN)
 
+# toot数を保存する
+hash = Hash.new(0)
+
 # 全tootを取得
 options = {limit: LIMIT}
 loop do
   response = client.statuses(ID, options)
   response.each do |res|
-    puts res.created_at + res.content
+    time = Time.parse(res.created_at).getlocal # UTC->JSTに変換
+    date = time.strftime("%Y-%m-%d")
+    hash[date] += 1
+    # puts "#{date}: #{res.content}"
   end
 
   min = response.min {|a, b| a.id <=> b.id }
-  puts "next_id: #{min.id}"
+  # puts "next_id: #{min.id}"
   options = {max_id: min.id, limit: LIMIT} 
 
   break if response.size < LIMIT
 end
 
+hash.each{ |key, value|
+  puts "#{key} -> #{value}"
+}
